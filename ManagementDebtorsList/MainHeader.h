f3f9ba5edb3823380703ec566ac8fce9f46e604c
Shmuel Safrin <shmuelsafrin@gmail.com>
2022-04-28 19:36:03 +0300

    test

--- ManagementDebtorsList/MainHeader.h
+++ ManagementDebtorsList/MainHeader.h
@@ -1,12 +1,12 @@
-#include "validations.h"
-#include "buildDeleteFuncs.h"
-#include "userFuncs.h"
-#include "utilities.h"
- 
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "validations.h"
+#include "buildDeleteFuncs.h"
+#include "userFuncs.h"
+#include "utilities.h"
 #define MAX_LINE 200
 char FileName[] = "DebtorsList.csv";
 
