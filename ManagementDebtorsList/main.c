d3c95298f3ff64dc651bdafad0dec91798a7e73f
Shmuel Safrin <shmuelsafrin@gmail.com>
2022-06-22 11:01:43 +0300

    date&hour

--- ManagementDebtorsList/main.c
+++ ManagementDebtorsList/main.c
@@ -4,11 +4,9 @@ char FileName[] = "DebtorsList.csv";
 
 int main()
 {
-	time_t seconds;
-	seconds = time(NULL);
-	struct tm* ptr;
-	ptr = localtime(&seconds);
-	printf("Local date & hour: %s", asctime(ptr));
+	time_t curtime;
+	time(&curtime);
+	printf("Local date & hour: %s", ctime(&curtime));
 	printf("\"Welcome to our program that summarizes all the debts of your debtors!\"\n");
 
 	Debtor* head = NULL;
